62f3fe2114d9a646d0279df3ec96d5178d8a78ae
AyaHelli <ayahilly_96hotmail.com>
2023-02-01 13:25:25 +0100

    4basic

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-let text = ("Hi my name is Martin");
+let text = "Hi my name is Martin";
+let players = ("Martin", "Thomas, Peter", "Mathias", "Niki")
+let sentence = "The most valuable player of the match is";
+console.log(sentence + " " + players[2] + ">");
