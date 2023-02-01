be692a7475d46aeebe3a1ef850f9d4ff160f53c5
AyaHelli <ayahilly_96hotmail.com>
2023-02-01 13:40:58 +0100

    ""change

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 let text = "Hi my name is Martin";
-let players = ("Martin", "Thomas, Peter", "Mathias", "Niki")
+let players = ["Martin", "Thomas", "Peter", "Mathias", "Niki"];
 let sentence = "The most valuable player of the match is";
-console.log(sentence + " " + players[2] + ">");
+console.log(sentence + " " + players[2]);
